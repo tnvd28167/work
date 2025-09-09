7be5a0c99a5b6361a7fc1c0272a40d881cda282e
tnvd28167 <minasyantigran2007@gmail.com>
2025-09-09 11:54:16 +0300

    Create practice4.cpp

---
+++ practice4.cpp
@@ -0,0 +1,14 @@
+// практика 4 
+#include <iostream>
+#include <string>
+
+
+int main()
+{ 
+    long long v, t;
+    std::cin >> v >> t;
+    long long s = (v * t) % 109;
+    std::cout << s;
+    
+  
+}
