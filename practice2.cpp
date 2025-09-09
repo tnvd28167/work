a18841d760ad02f66f32be80789c2f1a79dfe1a5
tnvd28167 <minasyantigran2007@gmail.com>
2025-09-09 11:52:52 +0300

    Create practice2.cpp

---
+++ practice2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{ 
+    long long a, b;
+    std::cin >> a >> b;
+    std::cout << a + b << "\n";
+    std::cout << a - b << "\n";
+    std::cout << a * b << "\n"; 
+    std::cout << a / b << "\n";
+}
